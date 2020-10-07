1b5777452a6d7f9e0f62c7f78f4dd07a4cd9da0a
Jorge Sánchez <jsanchezmanzano98@gmail.com>
2020-10-07 17:55:12 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 //Puerto
 //========================
 process.env.PORT = process.env.PORT || 3000;
+
+//========================
+//Entorno
+//========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========================
+//Base de datos
+//========================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://jsanchezmanzano:A361AdK9wD3ZxWGV@cluster0.lttwb.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
